bf6c577d81770e8d521a6b063f31651fa5ba41fd
ashikdas0002 <ashikdas0002@gmail.com>
2025-05-20 03:31:04 +0600

    Spinner Loader implement

--- app.js
+++ app.js
@@ -1,46 +1,78 @@
+const removeActiveBtn = () => {
+  const buttons = document.getElementsByClassName('category-btn');
+  for (let btn of buttons) {
+    btn.classList.remove('activeBtnStyle');
+    btn.classList.add('categoryBtnStyle_1');
+  }
+}
+
+
 const loadCategories = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
-    const data = await res.json();
-    displayCategories(data.categories);
+  const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
+  const data = await res.json();
+  displayCategories(data.categories);
+
 };
 
 const displayCategories = (categories) => {
 
-    const categoryContainer = document.getElementById('category-btn-container');
-    categories.forEach((item) => {
-        const { category, category_icon } = item;
-        const buttonContainer = document.createElement('div');
-        buttonContainer.innerHTML = `
+  const categoryContainer = document.getElementById('category-btn-container');
+  categories.forEach((item) => {
+    const { category, category_icon } = item;
+    const buttonContainer = document.createElement('div');
+    buttonContainer.innerHTML = `
         
-        <button class="categoryBtnStyle_1 gap-2 font-[Lato] mx-auto"> <span><img class="w-[40px] h-[40px]" src=${category_icon}/></span>  <span>${category}</span>  </button>
+        <button  id="btn-${category}" onclick="loadCategoriesVideo('${category}')" class="categoryBtnStyle_1  category-btn"> <span><img class="w - [40px] h - [40px]" src=${category_icon}/></span>  <span>${category}</span>  </button>
         
         `;
-        categoryContainer.appendChild(buttonContainer);
-    })
+    categoryContainer.appendChild(buttonContainer);
+  })
 
 };
 
 const loadPets = async () => {
-    try {
-        const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`);
-        const data = await res.json();
-        displayPets(data.pets);
-    }
-    catch (err) {
-        console.error('Get Error from Load pets', err);
-    }
+  try {
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`);
+    const data = await res.json();
+    displayPets(data.pets);
+  }
+  catch (err) {
+    console.error('Get Error from Load pets', err);
+  }
 };
 
 const displayPets = (pets) => {
-    const petsSection = document.getElementById('pets-card-section');
-    console.log(pets);
-    pets.forEach((pet) => {
+  const petsSection = document.getElementById('pets-card-section');
+  if (pets.length === 0) {
+    petsSection.classList.remove('grid');
+    petsSection.innerHTML = `
+    
 
-        const { price, pet_name, pet_details, image, date_of_birth, breed, gender } = pet;
-        const dateOfBirth = (date_of_birth === null || date_of_birth === undefined) ? 'Not Avilable' : date_of_birth;
+        <div class="bg-gray-100 rounded-[24px] py-[100px]"> 
+        
+        <div> <img class="mx-auto" src="images/error.webp"/> </div>
+        <h2 class="text-[#131313] text-center font-[Inter] text-[32px] font-bold">No Information Available </h2>
+        <p class="font-[Lato] font-normal text-[#13131370] text-center"> Pets are not avaiable right now please try another time </p>
+        
+        </div>
+    
+    
+    `;
+    return;
+  }
+  else {
 
-        const card = document.createElement('div');
-        card.innerHTML = `
+    petsSection.classList.add('grid');
+  }
+
+  petsSection.innerHTML = ""
+  pets.forEach((pet) => {
+
+    const { price, pet_name, pet_details, image, date_of_birth, breed, gender } = pet;
+    const dateOfBirth = (date_of_birth === null || date_of_birth === undefined) ? 'Not Avilable' : date_of_birth;
+
+    const card = document.createElement('div');
+    card.innerHTML = `
 
 
 
@@ -63,10 +95,10 @@ const displayPets = (pets) => {
       <rect width="20" height="20" fill="white"/>
     </clipPath>
   </defs>
-</svg></span> <span>Breed: ${breed === undefined ? 'Not Available' : breed} </span> </p>
+</svg></span> <span class="text-gray-400 text-[16px] font-normal">Breed: ${breed === undefined ? 'Not Available' : breed} </span> </p>
             <p class="flex items-center gap-1"><span><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
   <path d="M4.625 1.5V3.375M13.375 1.5V3.375M1.5 14.625V5.25C1.5 4.75272 1.69754 4.27581 2.04917 3.92417C2.40081 3.57254 2.87772 3.375 3.375 3.375H14.625C15.1223 3.375 15.5992 3.57254 15.9508 3.92417C16.3025 4.27581 16.5 4.75272 16.5 5.25V14.625M1.5 14.625C1.5 15.1223 1.69754 15.5992 2.04917 15.9508C2.40081 16.3025 2.87772 16.5 3.375 16.5H14.625C15.1223 16.5 15.5992 16.3025 15.9508 15.9508C16.3025 15.5992 16.5 15.1223 16.5 14.625M1.5 14.625V8.375C1.5 7.87772 1.69754 7.40081 2.04917 7.04917C2.40081 6.69754 2.87772 6.5 3.375 6.5H14.625C15.1223 6.5 15.5992 6.69754 15.9508 7.04917C16.3025 7.40081 16.5 7.87772 16.5 8.375V14.625" stroke="#5A5A5A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg> </span> <span>Birth: ${dateOfBirth} </span> </p>
+</svg> </span> <span  class="text-gray-400 text-[16px] font-normal">Birth: ${dateOfBirth} </span> </p>
             <p class="flex items-center gap-1"><span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
   <g opacity="0.7" clip-path="url(#clip0_2081_51)">
     <path d="M10 11.6666V17.5" stroke="#131313" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -79,7 +111,7 @@ const displayPets = (pets) => {
       <rect width="20" height="20" fill="white"/>
     </clipPath>
   </defs>
-</svg> </span> <span>Gender: ${gender === undefined ? 'Not Available' : gender} </span> </p>
+</svg> </span> <span  class="text-gray-400 text-[16px] font-normal">Gender: ${gender === undefined ? 'Not Available' : gender} </span> </p>
             <p class="flex items-center gap-1"><span> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
   <g clip-path="url(#clip0_2081_59)">
     <path d="M13.9167 6.66667C13.7508 6.19603 13.4479 5.7858 13.0469 5.48878C12.6459 5.19176 12.1652 5.02153 11.6667 5H8.33334C7.67029 5 7.03441 5.26339 6.56557 5.73223C6.09673 6.20107 5.83334 6.83696 5.83334 7.5C5.83334 8.16304 6.09673 8.79893 6.56557 9.26777C7.03441 9.73661 7.67029 10 8.33334 10H11.6667C12.3297 10 12.9656 10.2634 13.4344 10.7322C13.9033 11.2011 14.1667 11.837 14.1667 12.5C14.1667 13.163 13.9033 13.7989 13.4344 14.2678C12.9656 14.7366 12.3297 15 11.6667 15H8.33334C7.83479 14.9785 7.35409 14.8082 6.95311 14.5112C6.55213 14.2142 6.24921 13.804 6.08334 13.3333" stroke="#5A5A5A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -90,7 +122,7 @@ const displayPets = (pets) => {
       <rect width="20" height="20" fill="white"/>
     </clipPath>
   </defs>
-</svg></span> <span>Price: ${price}$</span> </p>
+</svg></span> <span class="text-gray-400 text-[16px] font-normal">Price: ${price}$</span> </p>
 
 <div class="border-b-2 border-gray-200 py-1 mb-3"> </div>
 
@@ -121,10 +153,39 @@ const displayPets = (pets) => {
 
         
         `;
-        petsSection.appendChild(card);
+    petsSection.appendChild(card);
 
-    });
+  });
+};
+
+const loadCategoriesVideo = async (category) => {
+
+  const petCards = document.getElementById('pets-card-section');
+  petCards.innerHTML = "";
+
+
+  const spinner = document.getElementById('spinner');
+
+  spinner.style.display = 'block';
+
+  setTimeout(async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${category}`)
+    const data = await res.json();
+    console.log(data.data);
+    removeActiveBtn();
+    const activebtn = document.getElementById(`btn-${category}`);
+    activebtn.classList.remove('categoryBtnStyle_1')
+    activebtn.classList.add('activeBtnStyle')
+    spinner.style.display = 'none';
+    displayPets(data.data);
+
+
+  }, 2000);
+
+
+
 }
 
 loadPets();
-loadCategories(); 
+loadCategories();
+
